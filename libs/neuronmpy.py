5501e291e00736c51d8faec7e9f363c2f9c7d9d6
shar <shaharkabasa@hotmail.com>
2025-12-09 21:17:15 +0200

    need to fix a bug with research on multiple data

--- libs/neuronmpy.py
+++ libs/neuronmpy.py
@@ -1,10 +1,10 @@
 import numpy as np
 import math
-
+from PIL import Image
 class neuronmpy:
 
-    def __init__(self, input_matrix, target_matrix, model_name, learning_rate):
-        self.input_matrix = input_matrix
+    def __init__(self,  target_matrix, model_name, learning_rate):
+        self.input_matrix = np.array([])
         self.target_matrix = target_matrix
         self.weight_matrix = np.array([])
         self.logitz_matrix = np.array([])
@@ -15,20 +15,28 @@ class neuronmpy:
         self.loss = 0
         self.error = []
         self.learning_rate = learning_rate
+
+        self.mode = "research"
     def createLayers(self, layeramount):
         size = self.input_matrix.shape
-        new_matrix = np.random.uniform(-1.0, 1.0,(size[1],size[1]))
+        scale = 1.0 / np.sqrt(size[1]) 
         
+        new_matrix = np.random.uniform(-0.01, 0.01, (size[1], 10))
+        
+        print(new_matrix.shape)
         self.weight_matrix = new_matrix
         if self.debug_mode:
+            print()
             print("Weight Matrix Populated")
         
     def debugMode(self):
         self.debug_mode = True
 
     def forwardpass(self):
-        
+        print(self.input_matrix)
         logitz = self.input_matrix @  self.weight_matrix
+
+        print(f"Logits result: {logitz}")
         self.logitz_matrix = logitz
         self.softmax()  
 
@@ -40,18 +48,21 @@ class neuronmpy:
         total = np.sum(self.probability_matrix)
         # Creating the P prectange for all arguments
         self.probability_matrix = self.probability_matrix / total
-
-        
-        self.lossfunction()
+        print(f"Logits result: {self.probability_matrix}")
+        if self.mode == "recognize":
+            self.getAnswer(self.probability_matrix)
+        if self.mode == "research":
+            self.lossfunction()
 
     def lossfunction(self):
-        highestprob = np.argmax(self.probability_matrix)
+       
         self.loss = -np.log(self.probability_matrix @ self.target_matrix[0])
         
-        print(f"Current loss: {self.loss}")
+        print(f"Current loss: {self.loss[0]}")
         self.calculateError()
     def calculateError(self):
-        self.error = self.probability_matrix - self.target_matrix    
+        self.error = self.probability_matrix - self.target_matrix
+        print(self.input_matrix.T)    
         self.gradient = self.input_matrix.T @ self.error
         self.calculateWeight() 
 
@@ -64,6 +75,8 @@ class neuronmpy:
             self.cleanData()
             self.forwardpass()
             tracker += 1
+        self.getAnswer(self.probability_matrix)
+        self.saveModel()
 
     def cleanData(self):
         self.loss = 0
@@ -71,3 +84,31 @@ class neuronmpy:
         self.probability_matrix = []
         
         
+    def loadimage(self, path_to_image):
+        img_data = Image.open(path_to_image).convert("L")
+        img_data = np.array(img_data ) 
+
+        if img_data.size > 0:
+            print("Data loaded succesfully")
+            print(img_data.size)
+            self.input_matrix = (img_data / 255.0).reshape(1, -1)
+            print(self.input_matrix.shape)
+        else:
+            print("Invaild path to image")
+
+    
+    def saveModel(self): 
+        np.save(f"models/{self.model_name}", self.weight_matrix)
+
+    def loadModel(self):
+        self.weight_matrix = np.load(f"models/{self.model_name}.npy")
+
+    def getAnswer(self, probablity_matrix):
+        answerList = ["A","B", "C", "D", "E", "F", "G", "H", "I", "J"]
+        indexWinner = np.argmax(probablity_matrix)
+        certainty = np.max(probablity_matrix) * 100
+        print(f"The machine predicts its an {answerList[indexWinner]} with  {certainty:0.2f} %")
+
+    def recognize(self):
+        self.mode = "recognize"
+        self.forwardpass()
--- playground.py
+++ playground.py
@@ -1,14 +1,31 @@
 from libs.neuronmpy import neuronmpy
 import numpy as np
-
+import random as rnd
 
 input_matrix = np.array([[0.25,0.75]])
 
-input_target = np.array([[1.0, 0]])
+input_target = np.array([[0.0, .0 ,0 ,0 ,0 ,0 ,0 ,0 ,0, 0]])
 
-newmodel = neuronmpy(input_matrix, input_target, "model001", 0.10)
+newmodel = neuronmpy(input_target, "model001", 0.00005)
 
-newmodel.debugMode()
+
+newmodel.loadimage("imgs/128x128B.jpeg")
+# newmodel.debugMode()
 newmodel.createLayers(1)
 
-newmodel.startResearch(100)
+
+newmodel.loadModel
+for i in range(1000):
+    x_random = rnd.randrange(0,1)
+    if x_random == 1: 
+        newmodel.loadimage("imgs/128x128B.jpeg")
+    else:
+        newmodel.loadimage("imgs/128x128A.jpeg")
+
+        newmodel.startResearch(1)
+
+
+
+# newmodel.loadModel()
+
+# newmodel.recognize()
